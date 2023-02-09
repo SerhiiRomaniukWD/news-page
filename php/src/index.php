566520c7dc291dc69b342029955b27883f3c073f
Serhii Romaniuk <deepspark@gmail.com>
2023-02-09 21:55:43 +0200

    add log in and register forms

--- php/src/index.php
+++ php/src/index.php
@@ -1,2 +1,37 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>News-page</title>
+  <link rel="stylesheet" href="assets/styles.css">
+</head>
+<body>
+  <main class="main">
+    <h1 class="title">Welcome to News portal!</h1>
 
+    <form class="form" "action="" method="">
+      <input
+        class="form_input"
+        type="text"
+        placeholder="Login"
+        required
+      >
+
+      <input
+        class="form_input"
+        type="password"
+        placeholder="Password"
+        required
+      >
+
+      <button class="form_button">Log in</button>
+
+      <p class="form_paragraph">
+        Don't have an account? ->
+        <a class="form_link" href="./register.php">register</a>
+        !
+      </p>
+    </form>
+  </main>
+</body>
+</html>
---
+++ php/src/register.php
@@ -0,0 +1,66 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>News-page</title>
+  <link rel="stylesheet" href="assets/styles.css">
+</head>
+<body>
+<main class="main">
+  <h1 class="title">Registration</h1>
+
+  <form class="form" "action="" method="">
+    <label class="form_label">Login</label>
+    <input
+      class="form_input"
+      type="text"
+      required
+    >
+
+    <label class="form_label">Name</label>
+    <input
+      class="form_input"
+      type="text"
+      required
+    >
+
+    <label class="form_label">Email</label>
+    <input
+      class="form_input"
+      type="email"
+      required
+    >
+
+    <label class="form_label">Password</label>
+    <input
+      class="form_input"
+      type="password"
+      required
+    >
+
+    <label class="form_label">Confirm password</label>
+    <input
+      class="form_input"
+      type="password"
+      required
+    >
+
+    <label class="form_label">Image</label>
+    <input
+      class="form_input"
+      type="file"
+      required
+    >
+
+    <button class="form_button">Log in</button>
+
+    <p class="form_paragraph">
+      Already registered? ->
+      <a class="form_link" href="./index.php">log in</a>
+      !
+    </p>
+  </form>
+</main>
+</body>
+</html>
